bc0fe8b6f188a09bc680dbf61434dcd02d34de7f
jshbaxter <jbaxter@robarts.ca>
2012-05-17 12:40:10 -0400

    Added utilities library which contains utilities such as the read-write lock and image pipe.

---
+++ vtkRobartsUtilities/vtkReadWriteLock.h
@@ -0,0 +1,52 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkReadWriteLock.h
+
+  Copyright (c) John SH Baxter, Robarts Research Institute
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkReadWriteLock - Video-for-Windows video digitizer
+// .SECTION Description
+// A lock that allows for multiple readers, but only one writer
+// a TCP/IP socket, allowing for multiple process VTK pipelines
+// .SECTION Caveats
+// Will only work as well as the underlying vtk mutices
+//
+
+#ifndef __VTKREADWRITELOCK_H
+#define __VTKREADWRITELOCK_H
+
+#include "vtkObject.h"
+#include "vtkMutexLock.h"
+
+class vtkReadWriteLock : public vtkObject {
+public:
+	static vtkReadWriteLock *New();
+
+	void ReaderLock();
+	void ReaderUnlock();
+	void WriterLock();
+	void WriterUnlock();
+
+protected:
+	vtkReadWriteLock();
+	~vtkReadWriteLock();
+	
+	vtkMutexLock* noWriters;
+	vtkMutexLock* noReaders;
+	vtkMutexLock* counter;
+	
+	unsigned int readerCount;
+
+private:
+
+
+
+};
+
+#endif
