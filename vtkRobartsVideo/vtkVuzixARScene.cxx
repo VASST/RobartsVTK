46f340012002c472b1882719f9ba9ec4eba4d42d
jshbaxter <jbaxter@robarts.ca>
2012-06-13 08:31:58 -0400

    FIX ARScene is now independent of the ARCamera, and has prototype logic for preserving background texture aspect ratio in rendering, necessary for AR project. (TODO Test anisotropic scaling and add logic for principle point offset.)

--- vtkRobartsVideo/vtkVuzixARScene.cxx
+++ vtkRobartsVideo/vtkVuzixARScene.cxx
@@ -1,5 +1,9 @@
 #include "vtkVuzixARScene.h"
+#include "vtkRenderWindow.h"
 #include "vtkObjectFactory.h"
+
+#include <math.h>
+#define PI 3.14159265
 
 vtkStandardNewMacro(vtkVuzixARScene);
 
@@ -8,10 +12,8 @@ vtkVuzixARScene::vtkVuzixARScene(){
 	//allocates the piece of the compilation
 	leftEyeRenderer = vtkRenderer::New();
 	rightEyeRenderer = vtkRenderer::New();
-	leftEyeCamera = vtkVuzixARCamera::New();
-	leftEyeRenderer->SetActiveCamera(leftEyeCamera);
-	rightEyeCamera = vtkVuzixARCamera::New();
-	rightEyeRenderer->SetActiveCamera(rightEyeCamera);
+	leftEyeCamera = leftEyeRenderer->GetActiveCamera();
+	rightEyeCamera = rightEyeRenderer->GetActiveCamera();
 
 	//allocate the texture bases for the renderers and link them together
 	leftEyePhysicalWorld = 0;
@@ -40,6 +42,24 @@ vtkVuzixARScene::vtkVuzixARScene(){
 	leftFocalPoint->PostMultiply();
 	rightFocalPoint = vtkTransform::New();
 	rightFocalPoint->PostMultiply();
+	leftScreenScale = vtkTransform::New();
+	rightScreenScale = vtkTransform::New();
+	
+	//initialize the screen sizes
+	IdealScreenSizeLeft[0] = 0;
+	IdealScreenSizeLeft[1] = 0;
+	IdealScreenSizeRight[0] = 0;
+	IdealScreenSizeRight[1] = 0;
+	ActualScreenSizeLeft[0] = 0;
+	ActualScreenSizeLeft[1] = 0;
+	ActualScreenSizeRight[0] = 0;
+	ActualScreenSizeRight[1] = 0;
+	leftIdealFocus = 1.0;
+	rightIdealFocus = 1.0;
+	leftPrinciplePoint[0] = 0.0;
+	leftPrinciplePoint[1] = 0.0;
+	rightPrinciplePoint[0] = 0.0;
+	rightPrinciplePoint[1] = 0.0;
 
 }
 
@@ -56,6 +76,8 @@ vtkVuzixARScene::~vtkVuzixARScene(){
 	rightFocalPoint->Delete();
 	deviceToLeftEye->Delete();
 	deviceToRightEye->Delete();
+	leftScreenScale->Delete();
+	rightScreenScale->Delete();
 }
 
 
@@ -71,7 +93,7 @@ void vtkVuzixARScene::Update(){
 
 	//use a reasonable value for the focal length (only matters for 
 	//resolution of the volume mapper)
-	double focalLength = 1.0;
+	double focalLength = 0.01;
 
 	//find the focal point of the left camera
 	leftFocalPoint->Identity();
@@ -107,23 +129,64 @@ void vtkVuzixARScene::Update(){
 	leftEyeCamera->SetFocalPoint( leftFocalPoint->GetPosition() );
 	rightEyeCamera->SetFocalPoint( rightFocalPoint->GetPosition() );
 
-	UpdateFrameSizes();
+	//cross-check the frame sizes
+	if( !IdealScreenSizeLeft[0] || !IdealScreenSizeLeft[1] ||
+		!IdealScreenSizeRight[0] || !IdealScreenSizeRight[1] )
+		UpdateFrameSizes();
+	
+	//update the left camera scaling
+	int* size = leftEyeRenderer->GetSize();
+	if( size[0] && size[1] && ( size[0] != ActualScreenSizeLeft[0] ||
+		size[1] != ActualScreenSizeLeft[1] ) ){
+		
+		//update stored screen size and calculate required aspect ratios
+		ActualScreenSizeLeft[0] = size[0];
+		ActualScreenSizeLeft[1] = size[1];
+		double aspectRatio = (double) size[0] / (double) size[1];
+		double idealAspectRatio = (double) IdealScreenSizeLeft[0] / (double) IdealScreenSizeLeft[1];
+		
+		//update camera scale matrix for anisotropic scaling (adjusting for screen size and principle point)
+		//TODO Add logic for the principle point
+		leftScreenScale->Identity();
+		leftScreenScale->Scale( idealAspectRatio / aspectRatio, 1, 1);
+	}
+
+	//update the right camera scaling
+	size = rightEyeRenderer->GetSize();
+	if( size[0] && size[1] && ( size[0] != ActualScreenSizeRight[0] ||
+		size[1] != ActualScreenSizeRight[1] ) ){
+		
+		//update stored screen size and calculate required aspect ratios
+		ActualScreenSizeRight[0] = size[0];
+		ActualScreenSizeRight[1] = size[1];
+		double aspectRatio = (double) size[0] / (double) size[1];
+		double idealAspectRatio = (double) ActualScreenSizeRight[0] / (double) ActualScreenSizeRight[1];
+
+		//update camera scale matrix for anisotropic scaling (adjusting for screen size and principle point)
+		//TODO Add logic for the principle point
+		rightScreenScale->Identity();
+		rightScreenScale->Scale( idealAspectRatio / aspectRatio, 1, 1);
+	}
 
 }
 
-//update the frame sizes on the cameras (used for determining the projection matrix)
+//update the frame sizes on the cameras (used for determining the projection matrix through the view angle)
 void vtkVuzixARScene::UpdateFrameSizes(){
 	if( leftEyePhysicalWorld ){
 		int extent[6];
 		this->leftEyePhysicalWorld->GetExtent( extent );
-		this->leftEyeCamera->SetFrameSize(	(double) (extent[1] - extent[0]+1),
-											(double) (extent[3] - extent[2]+1) );
+		IdealScreenSizeLeft[0] = extent[1] - extent[0]+1;
+		IdealScreenSizeLeft[1] = extent[3] - extent[2]+1;
+		double viewAngle = std::atan( 0.5 * (double)(IdealScreenSizeLeft[1]-1) / leftIdealFocus ) * 360.0 / PI;
+		this->leftEyeCamera->SetViewAngle(viewAngle);
 	}
 	if( rightEyePhysicalWorld ){
 		int extent[6];
 		this->rightEyePhysicalWorld->GetExtent( extent );
-		this->rightEyeCamera->SetFrameSize(	(double) (extent[1] - extent[0]+1),
-											(double) (extent[3] - extent[2]+1) );
+		IdealScreenSizeRight[0] = extent[1] - extent[0]+1;
+		IdealScreenSizeRight[1] = extent[3] - extent[2]+1;
+		double viewAngle = std::atan( 0.5 * (double)(IdealScreenSizeRight[1]-1) / rightIdealFocus ) * 360.0 / PI;
+		this->rightEyeCamera->SetViewAngle(viewAngle);
 	}
 }
 
@@ -138,12 +201,13 @@ vtkRenderer* vtkVuzixARScene::GetRightEyeView(){
 void vtkVuzixARScene::SetLeftEyeSource( vtkImageData* eye ){
 	this->leftEyeTexture->SetInput( (vtkDataObject*) eye );
 	leftEyePhysicalWorld = eye;
+	UpdateFrameSizes();
 }
 
 void vtkVuzixARScene::SetRightEyeSource( vtkImageData* eye ){
 	this->rightEyeTexture->SetInput( (vtkDataObject*) eye );
 	rightEyePhysicalWorld = eye;
-
+	UpdateFrameSizes();
 }
 
 void vtkVuzixARScene::SetTrackedTransform( vtkTransform* t){
@@ -164,12 +228,26 @@ void vtkVuzixARScene::SetLeftEyePixelwiseIntrinsicParameters(	double fx,
 																double fy,
 																double cx,
 																double cy ){
-	this->leftEyeCamera->SetPixelwiseIntrinsicParameters(fx,fy,cx,cy);
+	//get the relative focus in terms of the view angle
+	leftIdealFocus = 0.5 * (fx + fy);
+	double viewAngle = std::atan( 0.5 * (double)(IdealScreenSizeLeft[1]-1) / leftIdealFocus ) * 360.0 / PI;
+	this->leftEyeCamera->SetViewAngle(viewAngle);
+	
+	//save the principle point for later camera shifting
+	leftPrinciplePoint[0] = cx;
+	leftPrinciplePoint[1] = cy;
 }
 
 void vtkVuzixARScene::SetRightEyePixelwiseIntrinsicParameters(	double fx,
 																double fy,
 																double cx,
 																double cy ){
-	this->rightEyeCamera->SetPixelwiseIntrinsicParameters(fx,fy,cx,cy);
+	//get the relative focus in terms of the view angle
+	rightIdealFocus = 0.5 * (fx + fy);
+	double viewAngle = std::atan( 0.5 * (double)(IdealScreenSizeRight[1]-1) / rightIdealFocus ) * 360.0 / PI;
+	this->rightEyeCamera->SetViewAngle(viewAngle);
+
+	//save the principle point for later camera shifting
+	rightPrinciplePoint[0] = cx;
+	rightPrinciplePoint[1] = cy;
 }
