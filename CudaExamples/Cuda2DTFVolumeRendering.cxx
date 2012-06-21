9f43fff0597153e4b7b4ef5952788dd58c60aa07
jshbaxter <jbaxter@robarts.ca>
2012-06-21 18:44:34 -0400

    Added prototype of a multithreaded, CPU-based CT to US simulation in vtkRobartsUltrasound.

--- CudaExamples/Cuda2DTFVolumeRendering.cxx
+++ CudaExamples/Cuda2DTFVolumeRendering.cxx
@@ -1,118 +1,165 @@
+//
+///** @file main.cxx
+//*
+//*  @brief Sample volume ray casting program
+//*
+//*  @author John Stuart Haberl Baxter (Dr. Peter's Lab at Robarts Research Institute)
+//*  @note First documented on June 14, 2012
+//*
+//*/
+//
+////For ray-caster
+//#include "vtkCuda2DInExLogicVolumeMapper.h"
+//#include "vtkVolume.h"
+//#include "vtkVolumeProperty.h"
+//#include "vtkCuda2DTransferFunction.h"
+//#include "vtkCudaFunctionPolygon.h"
+//#include "vtkCudaFunctionPolygonReader.h"
+//#include "vtkBoxWidget.h"
+//#include "vtkCommand.h"
+//#include "vtkPlanes.h"
+//
+////general use
+//#include "vtkImageData.h"
+//#include "vtkMetaImageReader.h"
+//#include "vtkActor.h"
+//#include "vtkRenderer.h"
+//#include "vtkRenderWindow.h"
+//#include "vtkRenderWindowInteractor.h"
+//#include "vtkProperty.h"
+//#include "vtkCamera.h"
+//#include <vtkSmartPointer.h>
+//
+//// ---------------------------------------------------------------------------------------------------
+//// Callback for moving the planes from the box widget to the mapper
+//class vtkBoxWidgetKeyholeCallback : public vtkCommand
+//  {
+//  public:
+//    static vtkBoxWidgetKeyholeCallback *New()
+//      { return new vtkBoxWidgetKeyholeCallback; }
+//    virtual void Execute(vtkObject *caller, unsigned long, void*)
+//      {
+//      vtkBoxWidget *widget = reinterpret_cast<vtkBoxWidget*>(caller);
+//      if (this->Mapper)
+//        {
+//        vtkPlanes *planes = vtkPlanes::New();
+//        widget->GetPlanes(planes);
+//        this->Mapper->SetClippingPlanes(planes);
+//        planes->Delete();
+//        }
+//      }
+//    void SetMapper(vtkCuda2DInExLogicVolumeMapper* m) 
+//      { this->Mapper = m; }
+//
+//  protected:
+//    vtkBoxWidgetKeyholeCallback() 
+//      { this->Mapper = 0; }
+//
+//    vtkCuda2DInExLogicVolumeMapper *Mapper;
+//  };
+//
+//// ---------------------------------------------------------------------------------------------------
+//// Main Program
+//int main(int argc, char** argv){
+//
+//  //retrieve the image
+//  vtkSmartPointer< vtkMetaImageReader > imReader = 
+//    vtkSmartPointer< vtkMetaImageReader >::New();
+//  imReader->SetFileName("E:\\jbaxter\\data\\Chamberlain.mhd");
+//  imReader->Update();
+//
+//  //create the transfer function
+//  vtkSmartPointer< vtkCudaFunctionPolygonReader > tfReader = 
+//	  vtkSmartPointer< vtkCudaFunctionPolygonReader >::New();
+//  tfReader->SetFileName("E:\\jbaxter\\data\\Chamberlain.2tf");
+//  tfReader->Read();
+//  vtkSmartPointer< vtkCuda2DTransferFunction > func = 
+//	  vtkSmartPointer< vtkCuda2DTransferFunction >::New();
+//  for( int i = 0; i < tfReader->GetNumberOfOutputs(); i++ )
+//	  func->AddFunctionObject( tfReader->GetOutput(i) );
+//
+//  //assemble the ray caster
+//  vtkSmartPointer< vtkCuda2DInExLogicVolumeMapper > mapper = 
+//    vtkSmartPointer< vtkCuda2DInExLogicVolumeMapper >::New();
+//  mapper->SetInput( imReader->GetOutput() );
+//  mapper->SetVisualizationFunction( func );
+//  mapper->SetInExLogicFunction( func );
+//  mapper->SetGradientShadingConstants( 0.5 );
+//
+//  //assemble the VTK pipeline
+//  vtkSmartPointer< vtkVolume > volume = 
+//    vtkSmartPointer< vtkVolume >::New();
+//  volume->SetMapper( mapper );
+//
+//  vtkSmartPointer< vtkVolumeProperty > volProperty = 
+//    vtkSmartPointer< vtkVolumeProperty >::New();
+//  volume->Update();
+//
+//  vtkSmartPointer< vtkRenderer > renderer = 
+//    vtkSmartPointer< vtkRenderer >::New();
+//  renderer->AddVolume( volume );
+//  renderer->ResetCamera();
+//  renderer->SetBackground(1.0,1.0,1.0);
+//
+//  vtkSmartPointer< vtkRenderWindow > window = 
+//    vtkSmartPointer< vtkRenderWindow >::New();
+//  window->AddRenderer( renderer );
+//  window->Render();
+//
+//  //apply clipping planes widget and interactor
+//  vtkSmartPointer< vtkRenderWindowInteractor > interactor = 
+//    vtkSmartPointer< vtkRenderWindowInteractor >::New();
+//  interactor->SetRenderWindow( window );
+//
+//  //start the process
+//  interactor->Initialize();
+//  interactor->Start();
+//
+//  }
 
-/** @file main.cxx
-*
-*  @brief Sample volume ray casting program
-*
-*  @author John Stuart Haberl Baxter (Dr. Peter's Lab at Robarts Research Institute)
-*  @note First documented on June 14, 2012
-*
-*/
-
-//For ray-caster
-#include "vtkCuda2DInExLogicVolumeMapper.h"
-#include "vtkVolume.h"
-#include "vtkVolumeProperty.h"
-#include "vtkCuda2DTransferFunction.h"
-#include "vtkCudaFunctionPolygon.h"
-#include "vtkCudaFunctionPolygonReader.h"
-#include "vtkBoxWidget.h"
-#include "vtkCommand.h"
-#include "vtkPlanes.h"
-
-//general use
-#include "vtkImageData.h"
+#include "vtkSmartPointer.h"
+#include "vtkCT2USSimulation.h"
 #include "vtkMetaImageReader.h"
-#include "vtkActor.h"
+#include "vtkTransform.h"
+#include "vtkTexture.h"
+
 #include "vtkRenderer.h"
 #include "vtkRenderWindow.h"
-#include "vtkRenderWindowInteractor.h"
-#include "vtkProperty.h"
-#include "vtkCamera.h"
-#include <vtkSmartPointer.h>
-
-// ---------------------------------------------------------------------------------------------------
-// Callback for moving the planes from the box widget to the mapper
-class vtkBoxWidgetKeyholeCallback : public vtkCommand
-  {
-  public:
-    static vtkBoxWidgetKeyholeCallback *New()
-      { return new vtkBoxWidgetKeyholeCallback; }
-    virtual void Execute(vtkObject *caller, unsigned long, void*)
-      {
-      vtkBoxWidget *widget = reinterpret_cast<vtkBoxWidget*>(caller);
-      if (this->Mapper)
-        {
-        vtkPlanes *planes = vtkPlanes::New();
-        widget->GetPlanes(planes);
-        this->Mapper->SetClippingPlanes(planes);
-        planes->Delete();
-        }
-      }
-    void SetMapper(vtkCuda2DInExLogicVolumeMapper* m) 
-      { this->Mapper = m; }
-
-  protected:
-    vtkBoxWidgetKeyholeCallback() 
-      { this->Mapper = 0; }
-
-    vtkCuda2DInExLogicVolumeMapper *Mapper;
-  };
 
 // ---------------------------------------------------------------------------------------------------
 // Main Program
 int main(int argc, char** argv){
-
+	
   //retrieve the image
   vtkSmartPointer< vtkMetaImageReader > imReader = 
     vtkSmartPointer< vtkMetaImageReader >::New();
   imReader->SetFileName("E:\\jbaxter\\data\\Chamberlain.mhd");
   imReader->Update();
 
-  //create the transfer function
-  vtkSmartPointer< vtkCudaFunctionPolygonReader > tfReader = 
-	  vtkSmartPointer< vtkCudaFunctionPolygonReader >::New();
-  tfReader->SetFileName("E:\\jbaxter\\data\\Chamberlain.2tf");
-  tfReader->Read();
-  vtkSmartPointer< vtkCuda2DTransferFunction > func = 
-	  vtkSmartPointer< vtkCuda2DTransferFunction >::New();
-  for( int i = 0; i < tfReader->GetNumberOfOutputs(); i++ )
-	  func->AddFunctionObject( tfReader->GetOutput(i) );
+  
+  vtkSmartPointer< vtkTransform > probePose =
+	  vtkSmartPointer< vtkTransform >::New();
+  probePose->Identity();
 
-  //assemble the ray caster
-  vtkSmartPointer< vtkCuda2DInExLogicVolumeMapper > mapper = 
-    vtkSmartPointer< vtkCuda2DInExLogicVolumeMapper >::New();
-  mapper->SetInput( imReader->GetOutput() );
-  mapper->SetVisualizationFunction( func );
-  mapper->SetInExLogicFunction( func );
-  mapper->SetGradientShadingConstants( 0.5 );
+  vtkSmartPointer< vtkCT2USSimulation > ct2us =
+	  vtkSmartPointer< vtkCT2USSimulation >::New();
+  ct2us->SetInput( imReader->GetOutput() );
+  ct2us->SetTransform( probePose );
+  ct2us->SetOutputResolution(100,100,1);
+  ct2us->Modified();
+  ct2us->Update();
+  
+  vtkSmartPointer< vtkTexture > texture =
+	  vtkSmartPointer< vtkTexture >::New();
+  texture->SetInput(ct2us->GetOutput());
 
-  //assemble the VTK pipeline
-  vtkSmartPointer< vtkVolume > volume = 
-    vtkSmartPointer< vtkVolume >::New();
-  volume->SetMapper( mapper );
-
-  vtkSmartPointer< vtkVolumeProperty > volProperty = 
-    vtkSmartPointer< vtkVolumeProperty >::New();
-  volume->Update();
-
-  vtkSmartPointer< vtkRenderer > renderer = 
-    vtkSmartPointer< vtkRenderer >::New();
-  renderer->AddVolume( volume );
-  renderer->ResetCamera();
-  renderer->SetBackground(1.0,1.0,1.0);
-
-  vtkSmartPointer< vtkRenderWindow > window = 
-    vtkSmartPointer< vtkRenderWindow >::New();
+  vtkSmartPointer< vtkRenderer > renderer =
+	  vtkSmartPointer< vtkRenderer >::New();
+  renderer->SetBackgroundTexture(texture);
+  renderer->SetTexturedBackground(true);
+  vtkSmartPointer< vtkRenderWindow > window =
+	  vtkSmartPointer< vtkRenderWindow >::New();
   window->AddRenderer( renderer );
   window->Render();
-
-  //apply clipping planes widget and interactor
-  vtkSmartPointer< vtkRenderWindowInteractor > interactor = 
-    vtkSmartPointer< vtkRenderWindowInteractor >::New();
-  interactor->SetRenderWindow( window );
-
-  //start the process
-  interactor->Initialize();
-  interactor->Start();
-
-  }
+  ct2us->Update();
+}
