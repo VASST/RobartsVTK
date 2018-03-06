6f4711300396d189426eacdc76ec0b7802c7290b
Adam Rankin <adam.rankin@gmail.com>
2018-03-06 15:17:19 -0500

    Adding library install macro
    Using install macro for all libraries
    
    Cleaning up output directory population

--- CMake/RobartsCommon.cmake
+++ CMake/RobartsCommon.cmake
@@ -3,15 +3,15 @@
 
 # This macro generates a ...Export.h file that specifies platform-specific DLL export directives,
 # for example on Windows: __declspec( dllexport )
-MACRO(GENERATE_EXPORT_DIRECTIVE_FILE LIBRARY_NAME)
-  SET (MY_LIBNAME ${LIBRARY_NAME})
-  SET (MY_EXPORT_HEADER_PREFIX ${MY_LIBNAME})
-  SET (MY_LIBRARY_EXPORT_DIRECTIVE "${MY_LIBNAME}Export")
-  CONFIGURE_FILE(
+macro(GENERATE_EXPORT_DIRECTIVE_FILE LIBRARY_NAME)
+  set (MY_LIBNAME ${LIBRARY_NAME})
+  set (MY_EXPORT_HEADER_PREFIX ${MY_LIBNAME})
+  set (MY_LIBRARY_EXPORT_DIRECTIVE "${MY_LIBNAME}Export")
+  configure_file(
     ${RobartsVTK_Export_Template}
     ${CMAKE_CURRENT_BINARY_DIR}/${MY_EXPORT_HEADER_PREFIX}Export.h
     )
-ENDMACRO()
+endmacro()
 
 macro(REMOVE_VTK_DEFINITIONS)
   get_directory_property(_dir_defs DIRECTORY ${CMAKE_SOURCE_DIR} COMPILE_DEFINITIONS)
@@ -22,4 +22,22 @@ macro(REMOVE_VTK_DEFINITIONS)
       endif()
   endforeach()
   remove_definitions(${_vtk_definitions})
+endmacro()
+
+
+macro(RobartsVTKInstallLibrary _target_name _variable_root)
+  INSTALL(TARGETS ${_target_name} EXPORT RobartsVTK
+    RUNTIME DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY} COMPONENT RuntimeLibraries
+    LIBRARY DESTINATION ${CMAKE_LIBRARY_OUTPUT_DIRECTORY} COMPONENT RuntimeLibraries
+    ARCHIVE DESTINATION ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY} COMPONENT Development
+    )
+  INSTALL(FILES ${${_variable_root}_HDRS}
+    DESTINATION ${RobartsVTK_INCLUDE_INSTALL} COMPONENT Development
+    )
+  GET_TARGET_PROPERTY(_library_type ${_target_name} TYPE)
+  IF(${_library_type} STREQUAL SHARED_LIBRARY AND MSVC)
+    INSTALL(FILES "$<TARGET_PDB_FILE:${_target_name}>" OPTIONAL
+      DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY} COMPONENT RuntimeLibraries
+      )
+  ENDIF()
 endmacro()
