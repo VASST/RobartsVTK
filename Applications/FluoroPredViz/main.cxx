e6f86ddb30e95266fcb3d4f84f90006163f8bfd4
Adam Rankin <adam.rankin@gmail.com>
2018-03-23 15:48:26 -0400

    Removing missed patch change

--- Applications/FluoroPredViz/main.cxx
+++ Applications/FluoroPredViz/main.cxx
@@ -5,13 +5,8 @@
 // Local includes
 #include "FluoroPredViz.h"
 
-// VTK includes
-#include <QVTKOpenGLWidget.h>
-
 int main(int argc, char** argv)
 {
-  QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
-
   QApplication a(argc, argv);
   FluoroPredViz* w = new FluoroPredViz();
   if (w->GetSuccessInit())
